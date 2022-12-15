6f2af787c75062a594c6f338832dd1072315583c
Bibek Shah <90471455+bibekshhh@users.noreply.github.com>
2022-12-15 18:12:35 +0545

    Update date-subtracter.js

--- date-subtracter.js
+++ date-subtracter.js
@@ -5,7 +5,7 @@ const currentDate = new Date();
 
 function calcDate(prevDate, newDate) {
     const difference = ((new Date(newDate) - new Date(prevDate)) / 86400000).toFixed(1);
-    if (difference < 0) return "tero bau start date future date hunna"
+    if (difference < 0) return "start date cannot be a future date"
     return difference
 }
 
@@ -21,6 +21,7 @@ endDate.addEventListener('change', (e) => {
     if (startDate.value !== "" && startDate.value !== undefined && new Date(startDate.value) !== currentDate) {
         if (startDate.value <= e.target.value) {
             console.log(calcDate(startDate.value, e.target.value))
-        } else console.log("tero bau, ta janminu aghi marxas?")
+        } else console.log("end date cannot be before start date")
     }
+    console.log("Please select start date")
 });
