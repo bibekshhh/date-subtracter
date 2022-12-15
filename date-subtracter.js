a6d45e360841407abe9f8819408641125d6bacd9
Bibek Shah <90471455+bibekshhh@users.noreply.github.com>
2022-12-15 18:06:18 +0545

    Create date-subtracter.js

---
+++ date-subtracter.js
@@ -0,0 +1,26 @@
+const startDate = document.querySelector("#start");
+const endDate = document.querySelector("#end");
+
+const currentDate = new Date();
+
+function calcDate(prevDate, newDate) {
+    const difference = ((new Date(newDate) - new Date(prevDate)) / 86400000).toFixed(1);
+    if (difference < 0) return "tero bau start date future date hunna"
+    return difference
+}
+
+startDate.addEventListener('change', (e) => {
+    if (endDate.value !== "" && endDate.value !== undefined && new Date(endDate.value) !== currentDate) {
+        console.log(calcDate(e.target.value, endDate.value))
+    } else if (new Date(e.target.value) <= currentDate && e.target.value > endDate.value) {
+        console.log(calcDate(e.target.value, currentDate))
+    }
+});
+
+endDate.addEventListener('change', (e) => {
+    if (startDate.value !== "" && startDate.value !== undefined && new Date(startDate.value) !== currentDate) {
+        if (startDate.value <= e.target.value) {
+            console.log(calcDate(startDate.value, e.target.value))
+        } else console.log("tero bau, ta janminu aghi marxas?")
+    }
+});
